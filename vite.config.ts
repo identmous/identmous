2872b732e71b4fa5ede350fe21c3722255734668
yuimarudev <yuimarudev@outlook.jp>
2023-09-19 00:59:44 +0900

    working!

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
+const persistPath = resolve(new URL(import.meta.url).pathname, "..", ".wrangler", "state", "v3");
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit({
+      d1Databases: ["D1"],
+      d1Persist: resolve(persistPath, "d1"),
+      kvNamespaces: ["KV"],
+      kvPersist: resolve(persistPath, "kv")
+    }),
+  ],
   build: {
     sourcemap: true,
     minify: true,
